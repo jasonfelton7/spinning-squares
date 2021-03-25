a9755c05035b8617ecd791695d70d0a6d76c45ca
Jason <jfelton8899@gmail.com>
2021-03-25 12:02:13 -0400

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,22 @@
+function setup() {
+    createCanvas(windowWidth, windowHeight);
+    // Sets origin mode to be the rectangle's center
+    rectMode(CENTER);
+  }
+  
+  function draw() {
+    background(0);
+  
+    // Moves rectangle to center of canvas
+    translate(width / 2, height / 2);
+  
+    // Creates multiple rotating rectangles
+    for (let x = windowWidth/3; x > 0; x -= 20) {
+        // Makes height the same as width so it's a square
+        let y = x;
+        strokeWeight(0.5);
+        rotate(radians(frameCount / 200));
+        fill(75, x, 255);
+        rect(0, 0, x, y);
+    }
+  }
